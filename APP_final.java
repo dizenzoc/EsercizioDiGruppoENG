c7a8c386c6fa9dad6284b4dba9f37eb038ff8c2b
Di Zenzo Carmine Federico <114579198+dizenzoc@users.noreply.github.com>
2024-11-20 17:22:53 +0100

    App_final

---
+++ APP_final.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class APP_final {
+    Utente utenteCorrente;
+
+    public boolean login(String nome, String password, Registro registro) {
+        Utente utente = registro.trovaUtente(nome); //verifica se l'utente esiste nel registro e se le credenziali sono corrette.
+        if (utente != null && utente.login(nome, password)) {  // Se entrambe le condizioni sono soddisfatte
+            utenteCorrente = utente;  //imposta l'utente corrente (utenteCorrente)
+            return true; //restituisce true
+        }
+        return false; //, altrimenti restituisce false.
+    }
+
+    public void eseguiProva(Prova prova) {
+        if (utenteCorrente != null) { //verifico se esiste un utente loggato
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(prova.domanda()); //utilizzo uno Scanner per leggere la risposta dell'utente dalla console.
+            int risposta = scanner.nextInt();
+            if (prova.verificaRisposta(risposta)) {
+                utenteCorrente.punteggio = (utenteCorrente.punteggio() + 10); //Se la risposta è corretta, incrementa il punteggio dell'utente di 10 punti.
+                System.out.println("Risposta corretta! Punteggio: " + utenteCorrente.punteggio());
+            } else {
+                utenteCorrente.setPunteggio(utenteCorrente.getPunteggio() - 5); // Se la risposta è sbagliata, decrementa il punteggio dell'utente di 5 punti.
+                System.out.println("Risposta sbagliata. Punteggio: " + utenteCorrente.punteggio());
+            }
+        }
+    }
+
+    public void mostraPunteggio() { //mostro il punteggio all'utente
+        if (utenteCorrente != null) {
+            System.out.println("Punteggio attuale: " + utenteCorrente.punteggio());
+        }
+    }
+}
