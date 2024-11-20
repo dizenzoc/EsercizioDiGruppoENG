173072f7037eb16f3db6f5f42af40ae487e098a8
fromandre <AndreaRomeo24@icloud.com>
2024-11-20 17:09:50 +0100

    Update Registro.java

--- Registro.java
+++ Registro.java
@@ -1,3 +1,46 @@
+import java.util.ArrayList;
+
 public class Registro {
-    
+    ArrayList<Utente> utenti = new ArrayList();
+
+    public Registro(){
+        
+    }
+
+
+
+    //Metodo che controlla prende un oggetto Utente come parametro e controlla se il nome inserito è già esistente in un altro oggetto.
+    public void aggiungiutente(Utente tmp){
+        boolean flag = ricercautente(tmp);
+
+        if (!flag){
+            //SE IL NOME UTENTE NON ESISTE GIA' NELL'ARRAYLIST utenti INSERISCO IL NUOVO UTENTE
+            this.utenti.add(tmp);
+            return;
+        }
+            //ALTRIMENTI STAMPO UN ERRORE ED ESCO DALLA FUNZIONE 
+        else {
+            System.out.println("Utente già esistente! Seleziona un altro nome");
+            return;
+        }
+    }
+
+    public void rimuoviutente(Utente tmp){
+        boolean flag = ricercautente(tmp);
+        if (flag){
+            this.utenti.remove(tmp);
+        } else {
+            System.out.println("L'utente non è presente nel registro");
+        }
+    }
+
+    public boolean ricercautente(Utente tmp){
+        for (int i=0; i<this.utenti.size(); ++i){
+            if(tmp.nome.toLowercase().equals(this.utenti.get(i).nome.toLowercase()){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
